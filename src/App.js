a9dbd902a4ae8cb49fc9f624fd5840124fc46042
Daniel Ataieryo <daniel.ataiyero@jdplc.com>
2021-09-29 17:06:23 +0100

    test

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
 
   render(){
     const allUsers = this.state.users.map((user)=>{
-        return <h1 key={user.id}>{user.name} </h1>
+        return <h1 key={user.id}>{user.name} test </h1>
     });
 
     // let loading = this.state.loading
